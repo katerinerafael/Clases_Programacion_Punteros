b32b53fbefc9b599e901b2d80e7b6b64b2b2a1f7
katerinerafael <katerine.rafael@alumnos.uneatlantico.es>
2025-04-29 11:34:19 +0200

    Update Punteros.cpp

--- Punteros.cpp
+++ Punteros.cpp
@@ -1,20 +1,29 @@
 #include <iostream>
+#include "Punteros.h"
 
 using namespace std;
 
-int main(){
-
-    int Entero = 15;
-    int *PEntero = &Entero;
-    void *PPunteroGenerico = PEntero;
-
-    std::cout << "Entero " << Entero << std::endl;
-    std::cout << "*PEntero " << *PEntero << std::endl;
-    std::cout << "PEntero " << PEntero << std::endl;
-    
-    int *PInteger2 = (int *)PPunteroGenerico;
-    std::cout << "*PInteger2 " << *PInteger2 << std::endl;
-
-    return 0;
+// Función para mostrar el valor al que apunta un puntero genérico
+void mostrarValorPuntero(void *PPunteroGenerico) {
+    int *PInteger2 = (int *)PPunteroGenerico;  // Cast de void* a int*
+    cout << "*PInteger2: " << *PInteger2 << endl;
 }
 
+// Función para permutar los valores a los que apuntan dos punteros
+void permutar(int *a, int *b) {
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// Función para permutar las direcciones de dos punteros
+void permutarDirecciones(int *a, int *b) {
+    int *temp;
+    temp = a;
+    a = b;
+    b = temp;
+
+    // Imprimir las direcciones para demostrar el intercambio
+    cout << "Dentro de permutarDirecciones: a apunta a " << a << ", b apunta a " << b << endl;
+}
