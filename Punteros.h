47ea31c831e0c2e8157531803863bdf166e12117
katerinerafael <katerine.rafael@alumnos.uneatlantico.es>
2025-04-29 11:34:37 +0200

    Create Punteros.h

---
+++ Punteros.h
@@ -0,0 +1,9 @@
+#ifndef PUNTEROS_H
+#define PUNTEROS_H
+
+// Declaración de funciones
+void mostrarValorPuntero(void *PPunteroGenerico);
+void permutar(int *a, int *b); // Versión que intercambia los valores a los que apuntan
+void permutarDirecciones(int *a, int *b); // Versión que intercambia las direcciones de los punteros
+
+#endif // PUNTEROS_H
